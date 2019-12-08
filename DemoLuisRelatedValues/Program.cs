e8955fad95dff5589b08a082c5bdac02df4d65fb
Gassen <gassen.benhadj@outlook.com>
2019-12-08 01:12:11 +0100

    Update Program.cs

--- DemoLuisRelatedValues/Program.cs
+++ DemoLuisRelatedValues/Program.cs
@@ -1,4 +1,4 @@
-﻿using LuisAI.Dtos;
+using LuisAI.Dtos;
 using LuisAI.RelatedValues;
 using System;
 using System.Collections.Generic;
@@ -17,10 +17,18 @@ namespace DemoLuisRelatedValues
                 AppId = "<App Key>",
                 VersionId = "<App version e.g 0.1>",
                 SubsKey = "<Luis Subscription Key>",
+                //this is only used when you have already the ID
                 PhraseListId = "<Luis PhaseList ID>",
                 Endpoint = Region.WestEurope
             };
             ILuis luis = new Luis(luisParams);
+            
+            //// This step is one Time only to get a phaselist ID in order to use it as param for the  RelatedValuesAsync method
+            ////CreatePhaseListAsync method doesn't need the PhraseListId as property in the luisParams object
+            ////In case of lost of the phaseListID you can use the overrided method of CreatePhaseListAsync by passing a name for your phaselist. exmaple .CreatePhaseListAsync("phaseListTest")
+            //var phaseListID = await luis.CreatePhaseListAsync();
+            //Console.WriteLine(phaseListID);
+            
             while (repeat)
             {
                 List<string> keywords = new List<string>();
@@ -40,7 +48,9 @@ namespace DemoLuisRelatedValues
                 int number = int.Parse(Console.ReadLine());
 
                 Console.WriteLine("++++++++++++++++The related Values++++++++++++++++++");
-
+                
+                //RelatedValuesAsync method take as params keywords wich is List<string> and number of related values to get as result
+                //keywords could be one to * but, more that it better to give a list of maximum 3
                 var response = await luis.RelatedValuesAsync(keywords, number);
 
 
